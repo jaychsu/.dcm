a092b273876f9404a1424ab63198f131f8e4fec8
Jaych Su <jaychsu.tw@gmail.com>
2016-08-29 16:06:55 +0800

    Add main flow to build env

---
+++ install.zsh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+
+echo 'Please install [Xcode](http://developer.apple.com/xcode/) first.'
+open http://developer.apple.com/xcode/
+
+# pause
+read -n 1 -p "Press any key to continue..." INP
+if [[ $INP != '' ]]; then
+  echo -ne '\b \n'
+fi
+
+# Xcode command line tools
+ xcode-select --install
+
+./task/hardlink-config.zsh
+./task/setup-homebrew.zsh
+./task/setup-ruby.zsh
+./task/setup-node.zsh
