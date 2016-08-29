48a2bbc5ae7ed99f74dd63a69d0d9e07fab5d2ce
Jaych Su <jaychsu.tw@gmail.com>
2016-08-29 13:21:08 +0800

    Export owner info to global enviroment

--- dotfile/.zsh/variable.zsh
+++ dotfile/.zsh/variable.zsh
@@ -1,3 +1,9 @@
+# Owner Info
+export OWNER_NAME='Jaych Su'
+export OWNER_MAIL='jaychsu.tw@gmail.com'
+export GITHUB_ID='jaychsu'
+
+# Enviroment
 export PATH="$HOME/bin:/usr/local/bin:$PATH:$HOME/.rvm/bin"
 export EDITOR='vim'
 export PROXY_URL='http://127.0.0.1:10001'
