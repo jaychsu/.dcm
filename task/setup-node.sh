2abec878b18d7295169f0c3f1c4c3d0dfd25b473
Jaych Su <jaychsu.tw@gmail.com>
2016-08-30 16:42:02 +0800

    Run custom git command by system shell

---
+++ task/setup-node.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Install NVM
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.25.4/install.sh | bash
+
+. /usr/local/opt/nvm/nvm.sh
+
+# Install Node
+nvm install stable
+nvm use stable
+nvm alias default stable
+
+# Install useful tools
+npm install -g kneesocks webpack webpack-dev-server gulp yo generator-webapp
