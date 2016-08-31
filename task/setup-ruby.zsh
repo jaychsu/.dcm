3c5558f3f584f2f29edde99bdd008fa85d03d49d
Jaych Su <jaychsu.tw@gmail.com>
2016-08-31 09:32:49 +0800

    Fix error in flow

--- task/setup-ruby.zsh
+++ task/setup-ruby.zsh
@@ -1,17 +1,13 @@
 #!/bin/zsh
 
-# Only for download following things
-export HOMEBREW_GITHUB_API_TOKEN="e108260c652558a63fed18bbc3dd94d3df9a7474"
-
-# Install and Reload RVM
+# Third-party required
 brew install gnupg gnupg2
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+brew install libxml2 libxslt homebrew/dupes/libiconv libyaml
+
+# Install and Reload RVM
 curl -L https://raw.githubusercontent.com/wayneeseguin/rvm/master/binscripts/rvm-installer | bash -s stable
-
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
-
-# Third-party required by Rails
-brew install libxml2 libxslt libiconv libyaml
 
 # Switch download source to mirror from Ruby China
 echo "ruby_url=https://cache.ruby-china.org/pub/ruby" > ~/.rvm/user/db
