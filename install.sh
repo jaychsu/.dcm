1d08d281cc4aa40eee8d64e1735561e71431d51d
Jaych Su <jaychsu.tw@gmail.com>
2016-08-31 10:06:13 +0800

    End test

--- install.sh
+++ install.sh
@@ -5,22 +5,22 @@ set -e
 { # this ensures the entire script is downloaded #
 
 
-# command_exists() {
-#   type "$1" &> /dev/null
-# }
+command_exists() {
+  type "$1" &> /dev/null
+}
 
-# # Xcode command line tools
-# xcode-select --install || true
+# Xcode command line tools
+xcode-select --install || true
 
-# # Setup Homebrew
-# if command_exists brew; then
-#   brew update
-# else
-#   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-# fi
+# Setup Homebrew
+if command_exists brew; then
+  brew update
+else
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
-# brew doctor
-# brew install zsh zsh-completions git tree mongodb redis postgresql
+brew doctor
+brew install zsh zsh-completions git tree mongodb redis postgresql
 
 # Clone dotfiles
 cd && git clone https://github.com/jaychsu/.dcm.git
