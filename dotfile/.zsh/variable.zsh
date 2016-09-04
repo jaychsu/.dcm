d8dafd57b1e49a030dfc67211ae0c09d7508d11c
Jaych Su <jaychsu.tw@gmail.com>
2016-09-04 18:49:39 +0800

    Add env variables to config `Java`

--- dotfile/.zsh/variable.zsh
+++ dotfile/.zsh/variable.zsh
@@ -1,4 +1,5 @@
 # Enviroment
-export PATH="$HOME/bin:/usr/local/bin:/usr/local/sbin:$PATH:$HOME/.rvm/bin:/usr/local/opt/gnupg/libexec/gpgbin"
+export JAVA_HOME=$(/usr/libexec/java_home)
+export PATH="$HOME/bin:/usr/local/bin:/usr/local/sbin:$PATH:$HOME/.rvm/bin:$JAVA_HOME/bin:/usr/local/opt/gnupg/libexec/gpgbin"
 export EDITOR='vim'
 export PROXY_URL='http://127.0.0.1:10001'
