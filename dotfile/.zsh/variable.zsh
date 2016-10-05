344e5e871430037660382f37a5db9c3268e9ae8d
Jaych Su <jaychsu.tw@gmail.com>
2016-10-05 16:13:27 +0800

    Remove trivial setting

--- dotfile/.zsh/variable.zsh
+++ dotfile/.zsh/variable.zsh
@@ -1,7 +1,3 @@
-# Thrid-party
-# ======
-export JAVA_HOME=$(/usr/libexec/java_home)
-
 # Enviroment
 # ======
 export PATH="\
@@ -10,13 +6,6 @@ $HOME/bin:\
 /usr/local/sbin:\
 $PATH:\
 $HOME/.rvm/bin:\
-$JAVA_HOME/bin:\
 /usr/local/opt/gnupg/libexec/gpgbin\
 "
 export EDITOR='vim'
-
-# Proxy
-# ======
-PROXY_URL=http://127.0.0.1:6152
-export http_proxy=$PROXY_URL
-export https_proxy=$PROXY_URL
